4fb32c349429a14bc5d3afd20a707729a16586d6
Yudai MORIYA <s1520699@u.tsukuba.ac.jp>
2015-06-06 11:37:31 +0900

    spec3実装

--- recipe.c
+++ recipe.c
@@ -1,11 +1,24 @@
 #include<stdio.h>
 
-void spec1();
+void spec3();
 
-void main(){
-  spec1();
+int main(void){
+  spec3();
 }
 
-void spec1(){
-  printf("omuraisu\n");
+void spec3(){
+  FILE *recipedataFile;
+  char recipedata[256];
+
+  if((recipedataFile = fopen("recipe-data.txt", "r")) == NULL){
+    printf("file open error\n");
+    exit(0);
+  }
+
+  while(fgets(recipedata, 256, recipedataFile)){
+
+    printf("%s", recipedata);
+  }
+
+  fclose(recipedataFile);
 }
