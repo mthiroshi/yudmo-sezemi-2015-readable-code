50de8904ba1406e272b312ffb1b04b9e74f5ba1a
Yudai MORIYA <s1520699@u.tsukuba.ac.jp>
2015-06-06 12:00:53 +0900

    spec5を実装途中

--- recipe.c
+++ recipe.c
@@ -1,23 +1,28 @@
 #include<stdio.h>
 
-void spec4();
+void spec5();
 
 int main(void){
-  spec4();
+  spec5();
 }
 
-void spec4(){
+void spec5(){
   FILE *recipedataFile;
-  char recipedata[256];
+  char recipedataName[256];
+  int numofRecipedata = 3;
+  char recipedata[numofRecipedata][256];
+  int recipe_id = 0;
 
   if((recipedataFile = fopen("recipe-data.txt", "r")) == NULL){
     printf("file open error\n");
     exit(0);
   }
 
-  while(fgets(recipedata, 256, recipedataFile)){
+  while(fgets(recipedataName, 256, recipedataFile)){
 
-    printf("%s", recipedata);
+    //char型配列に文字列を代入する方法を検討中
+    recipedata[recipe_id][] = recipedataName;
+    printf("%s", recipedata[recipe_id][]);
   }
 
   fclose(recipedataFile);
